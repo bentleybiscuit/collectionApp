0e305b91a9ae42352f39ef1a249b63e84b2a142f
bentleybiscuit <charlottenightingale@ymail.com>
2019-09-26 13:03:24 +0100

    added required to addItem.php inputs

--- addItem.php
+++ addItem.php
@@ -10,9 +10,9 @@
 
 <form method="post" action="connect.php">
     <label for="common">Common Name: </label>
-    <input id=“common“ type="text" name="commName">
+    <input id=“common“ type="text" name="commName" required>
     <label for="scientific">Scientific Name: </label>
-    <input id="scientific" type="text" name="sciName">
+    <input id="scientific" type="text" name="sciName" required>
     <label for="kingdom">Kingdom: </label>
     <select id=“kingdom" name="king">
     <option value=“Animals”>Animals</option>
@@ -23,7 +23,7 @@
     <option value=“Protists”>Protists</option>
     </select>
     <label for="genome">Genome Size Mbp: </label>
-    <input id=“genome“ type="number" step=“0.01” min=“1" name="genSize">
+    <input id=“genome“ type="number" step=“0.01” min=“1" name="genSize" required>
     <input type="submit" value="Submit”>
         </form>
     </body>
